5314c55bb670b4b6dbe6c1a5b6c2b31099dad7bb
KahhorovSh04 <shaxzodqaxxorov04@gmail.com>
2021-12-16 10:23:15 +0500

    Library page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,26 @@
 // Dependencies
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { lightTheme, darkTheme } from './muiConfig'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// Pages
+import Home from './pages/Home'
+import Library from './pages/Library'
+// MUI
 import { ThemeProvider } from '@mui/material/styles'
-// Components
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Courses from './components/Courses'
-import Features from './components/Features'
-import Contacts from './components/Contacts'
-import Teachers from './components/Teachers'
-import Footer from './components/Footer'
-// Modals
-import CourseModal from './components/CourseModal'
-import MenuModal from './components/MenuModal'
-import InstructorModal from './components/InstructorModal'
+// Config
+import { lightTheme, darkTheme } from './muiConfig'
 
 function App() {
-  const [currentCourse, setCurrentCourse] = useState({
-    name: 'General English',
-    description:
-      'Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Phasellus molestie enim.',
-    banner: 'general-english.png',
-    duration: '3 months',
-    requirement: 'Elementary',
-  })
-  const [curVideo, setCurVideo] = useState('teacher1.mp4')
-  const [isVideoOpen, setIsVideoOpen] = useState(false)
   const [theme, setTheme] = useState(
     localStorage.getItem('theme') === 'dark' ? darkTheme : lightTheme
   )
-  const [openMenu, setOpenMenu] = useState(false)
-  const [openModal, setOpenModal] = useState(false)
-  const [isMobile, setIsMobile] = useState(
-    window.innerWidth <= 600 ? true : false
-  )
-  const [isTablet, setIsTablet] = useState(
-    window.innerWidth > 600 && window.innerWidth <= 1200 ? true : false
-  )
-
-  window.addEventListener('resize', () => {
-    setIsMobile(window.innerWidth <= 600 ? true : false)
-    setIsTablet(
-      window.innerWidth > 600 && window.innerWidth <= 1024 ? true : false
-    )
-  })
-
-  useEffect(() => {
-    let themeColor = theme === darkTheme ? 'dark' : 'light'
-    localStorage.setItem('theme', themeColor)
-  })
-
   return (
     <>
       <Router>
         <ThemeProvider theme={theme}>
-          <Navbar
-            setOpenMenu={setOpenMenu}
-            theme={theme}
-            setTheme={setTheme}
-            lightTheme={lightTheme}
-            darkTheme={darkTheme}
-            isMobile={isMobile}
-            isTablet={isTablet}
-          />
-          <Header isMobile={isMobile} isTablet={isTablet} />
-          <Courses
-            isTablet={isTablet}
-            isMobile={isMobile}
-            setOpenModal={setOpenModal}
-            setCurrentCourse={setCurrentCourse}
-          />
-          <Features isMobile={isMobile} />
-          <Teachers setCurVideo={setCurVideo} setIsVideoOpen={setIsVideoOpen} />
-          <Contacts />
-          <Footer theme={theme} lightTheme={lightTheme} darkTheme={darkTheme} />
-          <CourseModal
-            openModal={openModal}
-            setOpenModal={setOpenModal}
-            currentCourse={currentCourse}
-          />
-          <MenuModal
-            openMenu={openMenu}
-            setOpenMenu={setOpenMenu}
-            theme={theme}
-            setTheme={setTheme}
-            lightTheme={lightTheme}
-            darkTheme={darkTheme}
-          />
-          <InstructorModal
-            isVideoOpen={isVideoOpen}
-            setIsVideoOpen={setIsVideoOpen}
-            curVideo={curVideo}
-          />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/library' element={<Library />} />
+          </Routes>
         </ThemeProvider>
       </Router>
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react'
+// Components
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+import Courses from '../components/Courses'
+import Features from '../components/Features'
+import Contacts from '../components/Contacts'
+import Teachers from '../components/Teachers'
+import Footer from '../components/Footer'
+// Modals
+import MenuModal from '../components/MenuModal'
+import InstructorModal from '../components/InstructorModal'
+import CourseModal from '../components/CourseModal'
+// MUI
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark'
+import { Stack } from '@mui/material'
+// Config
+import { lightTheme, darkTheme } from '../muiConfig'
+
+const style = {
+  position: 'fixed',
+  bottom: '2%',
+  right: '2%',
+  width: '3.6rem',
+  height: '3.6rem',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '50%',
+  border: '2px solid #fff',
+  cursor: 'pointer',
+}
+
+const Home = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') === 'dark' ? darkTheme : lightTheme
+  )
+  const [currentCourse, setCurrentCourse] = useState({
+    name: 'General English',
+    description:
+      'Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Phasellus molestie enim.',
+    banner: 'general-english.png',
+    duration: '3 months',
+    requirement: 'Elementary',
+  })
+  const [curVideo, setCurVideo] = useState('teacher1.mp4')
+  const [isVideoOpen, setIsVideoOpen] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth <= 600 ? true : false
+  )
+  const [isTablet, setIsTablet] = useState(
+    window.innerWidth > 600 && window.innerWidth <= 1200 ? true : false
+  )
+
+  window.addEventListener('resize', () => {
+    setIsMobile(window.innerWidth <= 600 ? true : false)
+    setIsTablet(
+      window.innerWidth > 600 && window.innerWidth <= 1024 ? true : false
+    )
+  })
+
+  useEffect(() => {
+    let themeColor = theme === darkTheme ? 'dark' : 'light'
+    localStorage.setItem('theme', themeColor)
+  })
+
+  return (
+    <>
+      <Navbar
+        setOpenMenu={setOpenMenu}
+        theme={theme}
+        setTheme={setTheme}
+        lightTheme={lightTheme}
+        darkTheme={darkTheme}
+        isMobile={isMobile}
+        isTablet={isTablet}
+      />
+      <Header isMobile={isMobile} isTablet={isTablet} />
+      <Courses
+        isTablet={isTablet}
+        isMobile={isMobile}
+        setOpenModal={setOpenModal}
+        setCurrentCourse={setCurrentCourse}
+      />
+      <Features isMobile={isMobile} />
+      <Teachers setCurVideo={setCurVideo} setIsVideoOpen={setIsVideoOpen} />
+      <Contacts />
+      <Footer theme={theme} lightTheme={lightTheme} darkTheme={darkTheme} />
+      <CourseModal
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        currentCourse={currentCourse}
+      />
+      <MenuModal
+        openMenu={openMenu}
+        setOpenMenu={setOpenMenu}
+        theme={theme}
+        setTheme={setTheme}
+        lightTheme={lightTheme}
+        darkTheme={darkTheme}
+      />
+      <InstructorModal
+        isVideoOpen={isVideoOpen}
+        setIsVideoOpen={setIsVideoOpen}
+        curVideo={curVideo}
+      />
+      <Stack style={style} backgroundColor='primary.main'>
+        <QuestionMarkIcon
+          sx={{
+            color: 'primary.contrastText',
+            fontSize: '2rem',
+          }}
+        />
+      </Stack>
+    </>
+  )
+}
+
+export default Home
